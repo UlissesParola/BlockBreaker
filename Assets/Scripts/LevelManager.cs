e74fda7651b2644765504d22675382b5466cd436
Ulisses Parola <ulissesmt@gmail.com>
2017-08-14 19:16:42 -0300

    scenes transition and music

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string scene)
+	{
+		SceneManager.LoadScene(scene);
+	}	
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}	
+}
