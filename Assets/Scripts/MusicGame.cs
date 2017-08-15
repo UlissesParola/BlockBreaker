22fd55dbfaae4f3dc8107968a90eec7f389a9c3c
Ulisses Parola <ulissesmt@gmail.com>
2017-08-14 21:53:30 -0300

    preparing the play scene

--- Assets/Scripts/MusicGame.cs
+++ Assets/Scripts/MusicGame.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicGame : MonoBehaviour {
+	public static MusicGame instance;
+
+	void Awake(){
+		if (instance != null) 
+		{
+			Destroy (gameObject);
+		} 
+		else 
+		{
+			instance = this;	
+			DontDestroyOnLoad(gameObject);
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
-		DontDestroyOnLoad(gameObject);
+
+
 	}
 	
 	// Update is called once per frame
