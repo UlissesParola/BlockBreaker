86db40fcb4b3c8a3b6b59213030fa0613d765eb7
Ulisses Parola <ulissesmt@gmail.com>
2017-08-15 23:13:17 -0300

    include a ball and colliders

---
+++ Assets/Scripts/Bottom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottom : MonoBehaviour {
+
+	public LevelManager levelManager;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		levelManager.LoadLevel ("Lose");
+	}
+
+}
